477a7d0a16ce297f6b19ecddeb84b939c8dc239d
Chris <sihrc.c.lee@gmail.com>
2014-09-30 21:38:45 -0400

    FIX: Firebase issues

--- app/src/main/java/com/example/root/myapplication2/ChatObject.java
+++ app/src/main/java/com/example/root/myapplication2/ChatObject.java
@@ -9,23 +9,14 @@ import java.io.Serializable;
  * Created by clee2 on 5/26/2014.
  */
 public class ChatObject implements Serializable {
-    String sender, message, timestamp;
+    public String name;
+    public String message;
+    public String timestamp;
 
-    public ChatObject (String sender, String message, String timestamp) {
-        this.sender = sender;
+    public ChatObject(){}
+    public ChatObject(String message, String name, String timestamp) {
+        this.name = name;
         this.message = message;
         this.timestamp = timestamp;
-    }
-
-    public String getSender() {
-        return sender;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getTimestamp() {
-        return timestamp;
     }
 }
